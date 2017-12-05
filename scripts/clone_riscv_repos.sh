efb870e1f135ce66cd2227d1f5c79bda728ad00c
Edmond Cote <edmond@pnlsys.com>
2017-12-05 00:34:23 +0000

    cd to $RISCV_CI

---
+++ scripts/clone_riscv_repos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPOS=$(cat <<EOM
+https://github.com/riscv/riscv-tools.git 
+https://github.com/riscv/riscv-qemu.git
+https://github.com/riscv/riscv-linux.git
+https://github.com/riscv/riscv-pk.git
+https://github.com/riscv/riscv-gnu-toolchain
+https://github.com/riscv/riscv-isa-sim
+https://github.com/riscv/riscv-fesvr
+EOM
+)
+
+for r in $REPOS; do
+    cd $RISCV_CI && git clone --recursive $r
+done
+

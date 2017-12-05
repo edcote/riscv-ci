0ad25ff9267d930c25650a55849c2ea2ca5eebe8
Edmond Cote <edmond.cote@gmail.com>
2017-12-04 16:40:56 -0800

    Update clone_riscv_repos.sh

--- scripts/clone_riscv_repos.sh
+++ scripts/clone_riscv_repos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 REPOS=$(cat <<EOM
-https://github.com/riscv/riscv-tools.git 
-https://github.com/riscv/riscv-qemu.git
-https://github.com/riscv/riscv-linux.git
-https://github.com/riscv/riscv-pk.git
-https://github.com/riscv/riscv-gnu-toolchain
-https://github.com/riscv/riscv-isa-sim
-https://github.com/riscv/riscv-fesvr
+https://github.com/riscv/riscv-tools.git         -o riscv-tools
+https://github.com/riscv/riscv-qemu.git          -o riscv-qemu
+https://github.com/riscv/riscv-linux.git         -o riscv-linux
+https://github.com/riscv/riscv-pk.git            -o riscv-pk
+https://github.com/riscv/riscv-gnu-toolchain.git -o riscv-gnu-toolchain
+https://github.com/riscv/riscv-isa-sim.git       -o riscv-isa_sim
+https://github.com/riscv/riscv-fesvr.git         -o riscv-fesvr
 EOM
 )
 

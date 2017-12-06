98644349a6edb7e7e77eaef70cbbd3943fe22501
Edmond Cote <edmond.cote@gmail.com>
2017-12-06 15:08:54 -0800

    development in progress

--- src/jobdsl/example.groovy
+++ src/jobdsl/example.groovy
@@ -1,9 +1,16 @@
+import com.here.gradle.plugins.jobdsl.util.GroovySeedJobBuilder
+
+// See: https://github.com/heremaps/gradle-jenkins-jobdsl-plugin#writing-job-dsl-scripts
+
+
+def x = new GroovySeedJobBuilder()
+
 def riscvURL = "https://github.com/riscv/"
 def freechipsURL = "https://github.com/freechips/"
 
 job("pk") {
-  scm {
-    git(riscvURL+"/riscv-pk.git")
-  }
+    scm {
+        git(riscvURL + "/riscv-pk.git")
+    }
 }
 

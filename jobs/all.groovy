1cbe0a9391cb1c537004eeb85bab1065e17fccac
Edmond Cote <edmond.cote@gmail.com>
2017-12-07 14:07:26 -0800

    all.groovy

--- jobs/all.groovy
+++ jobs/all.groovy
@@ -1,4 +1,16 @@
-
-
-job('tutorial') {
+class Builder {
+    static github(dslFactory, jobName, ownerAndTarget) {
+        dslFactory.job(jobName) {
+            scm {
+                github(ownerAndTarget)
+            }
+        }
+        steps {
+            shell('/bin/true')
+        }
+    }
 }
+
+Builder.github(this, 'pk', 'riscv/riscv-pk')
+Builder.github(this, 'fesvr', 'riscv/riscv-fesvr')
+Builder.github(this, 'spike', 'riscv/riscv-isa-sim')

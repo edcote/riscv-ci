f0ee1447ade9dd2993ac9aa7097d9678b60f3111
Edmond Cote <edmond.cote@gmail.com>
2017-12-11 12:42:30 -0800

    Initial version

---
+++ jobs/jobgen.py
@@ -0,0 +1,55 @@
+#!/usr/env python3
+
+import json
+
+jsonfile = "/home/edc/gitrepos/riscv-ci/jobs/jobspec.json"
+
+jobspec = json.load(open(jsonfile))
+
+# master job
+jobs = []
+for job in jobspec:
+    jobs.append(
+        "    jobs[{}] = {{ build job: [{}], parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:\"${{env.WORKSPACE}}\"]] }}".format(
+            job, job))
+
+f = open('../pipelines/master_pipeline.groovy', 'w')
+f.write("""\
+node {{
+    def jobs = [:]
+{}
+    parallel jobs
+}}""".format('\n'.join(jobs)))
+f.close()
+
+# pipeline jobs
+for job in jobspec:
+    f = open('../pipelines/{}_pipeline.groovy'.format(job), 'w')
+    f.write("""\
+node {{
+stage('Build') {{
+    sh('echo WORKSPACE: $WORKSPACE')
+    sh('echo RISCV_CI: $RISCV_CI')
+    script('"${{env.WORKSPACE}}"/pipelines/{}_build.groovy')
+    sh('sleep 2s')
+}}        
+stage('Test') {{
+    sh('echo WORKSPACE: $WORKSPACE')
+    sh('echo RISCV_CI: $RISCV_CI')
+    script('"${{env.WORKSPACE}}"/pipelines/{}_test.groovy')
+    sh('sleep 2s')
+}}
+}}
+    """.format(job, job))
+    f.close
+
+# dummy jobs
+for job in jobspec:
+    for stage in ["build", "test"]:
+        f = open('../pipelines/{}_{}.groovy'.format(job, stage), 'w')
+        f.write("""\
+sh('echo WORKSPACE: $WORKSPACE')
+sh('echo RISCV_CI: $RISCV_CI')       
+sh('/bin/true')       
+            """)
+        f.close()

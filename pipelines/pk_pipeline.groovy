8de3ec79b219a3c2fe092fc7cf94da1a9c7abbd7
Edmond Cote <edmond.cote@gmail.com>
2017-12-11 16:09:35 -0800

    remove " syntax error

--- pipelines/pk_pipeline.groovy
+++ pipelines/pk_pipeline.groovy
@@ -2,13 +2,13 @@ node {
 stage('Build') {
     sh('echo WORKSPACE: $WORKSPACE')
     sh('echo RISCV_CI: $RISCV_CI')
-    script("${env.WORKSPACE}"/pipelines/pk_build.groovy")
+    script("${env.WORKSPACE}/pipelines/pk_build.groovy")
     sh('sleep 2s')
 }        
 stage('Test') {
     sh('echo WORKSPACE: $WORKSPACE')
     sh('echo RISCV_CI: $RISCV_CI')
-    script("${env.WORKSPACE}"/pipelines/pk_test.groovy")
+    script("${env.WORKSPACE}/pipelines/pk_test.groovy")
     sh('sleep 2s')
 }
 }
--- pipelines/rocketchip_pipeline.groovy
+++ pipelines/rocketchip_pipeline.groovy
@@ -2,13 +2,13 @@ node {
 stage('Build') {
     sh('echo WORKSPACE: $WORKSPACE')
     sh('echo RISCV_CI: $RISCV_CI')
-    script("${env.WORKSPACE}"/pipelines/rocketchip_build.groovy")
+    script("${env.WORKSPACE}/pipelines/rocketchip_build.groovy")
     sh('sleep 2s')
 }        
 stage('Test') {
     sh('echo WORKSPACE: $WORKSPACE')
     sh('echo RISCV_CI: $RISCV_CI')
-    script("${env.WORKSPACE}"/pipelines/rocketchip_test.groovy")
+    script("${env.WORKSPACE}/pipelines/rocketchip_test.groovy")
     sh('sleep 2s')
 }
 }

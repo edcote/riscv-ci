e990bb1622c1209f28360e4317d8480dcd19f1ee
Edmond Cote <edmond.cote@gmail.com>
2018-01-14 18:26:48 +0000

    add openocd

--- pipelines/master_pipeline.groovy
+++ pipelines/master_pipeline.groovy
@@ -3,10 +3,11 @@ node {
     sh("echo RISCV_CI: ${env.RISCV_CI}")
     def jobs = [:]
     jobs['rocketchip'] = { stage('rocketchip') { build job: 'rocketchip', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
-    jobs['qemu'] = { stage('qemu') { build job: 'qemu', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
-    jobs['toolchain'] = { stage('toolchain') { build job: 'toolchain', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
-    jobs['spike'] = { stage('spike') { build job: 'spike', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
     jobs['pk'] = { stage('pk') { build job: 'pk', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
     jobs['fesvr'] = { stage('fesvr') { build job: 'fesvr', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
+    jobs['spike'] = { stage('spike') { build job: 'spike', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
+    jobs['qemu'] = { stage('qemu') { build job: 'qemu', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
+    jobs['openocd'] = { stage('openocd') { build job: 'openocd', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
+    jobs['toolchain'] = { stage('toolchain') { build job: 'toolchain', parameters: [[$class: 'StringParameterValue', name: 'RISCV_CI', value:"${env.RISCV_CI}"]] } }
     parallel jobs
 }

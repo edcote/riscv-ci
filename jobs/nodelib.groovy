a4de4528a6ee5ca959ebf156d1efe7987ea863fa
Edmond Cote <edmond.cote@gmail.com>
2018-01-31 16:25:39 +0000

    update generated file

--- jobs/nodelib.groovy
+++ jobs/nodelib.groovy
@@ -1,136 +1,163 @@
 // DO NOT EDIT, MANAGED FILE
-def toolchain_build() {
-    sh("""
-/bin/true
-""")
-}
-def toolchain_test() {
-    sh("""
-/bin/true
-""")
-}
-def toolchain_deploy() {
-    sh("""
-/bin/true
-""")
-}
-def spike_build() {
-    sh("""
-/bin/true
-""")
-}
-def spike_test() {
-    sh("""
-/bin/true
-""")
-}
-def spike_deploy() {
-    sh("""
-/bin/true
-""")
-}
-def linux_build() {
-    sh("""
-/bin/true
-""")
-}
-def linux_test() {
-    sh("""
-/bin/true
-""")
-}
-def linux_deploy() {
-    sh("""
-/bin/true
-""")
-}
-def openocd_build() {
-    sh("""
-/bin/true
-""")
-}
-def openocd_test() {
-    sh("""
-/bin/true
-""")
-}
-def openocd_deploy() {
-    sh("""
-/bin/true
-""")
-}
 def tests_build() {
     sh("""
+echo 'Build' && 
 /bin/true
 """)
 }
 def tests_test() {
     sh("""
+echo 'Test' && 
 /bin/true
 """)
 }
 def tests_deploy() {
     sh("""
-/bin/true
-""")
-}
-def fesvr_build() {
-    sh("""
-/bin/true
-""")
-}
-def fesvr_test() {
-    sh("""
-/bin/true
-""")
-}
-def fesvr_deploy() {
-    sh("""
-/bin/true
-""")
-}
-def rocketchip_build() {
-    sh("""
-/bin/true
-""")
-}
-def rocketchip_test() {
-    sh("""
-/bin/true
-""")
-}
-def rocketchip_deploy() {
-    sh("""
-/bin/true
-""")
-}
-def qemu_build() {
-    sh("""
-/bin/true
-""")
-}
-def qemu_test() {
-    sh("""
-/bin/true
-""")
-}
-def qemu_deploy() {
-    sh("""
+echo 'Deploy' && 
 /bin/true
 """)
 }
 def pk_build() {
     sh("""
+echo 'Build' && 
 /bin/true
 """)
 }
 def pk_test() {
     sh("""
+echo 'Test' && 
 /bin/true
 """)
 }
 def pk_deploy() {
     sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def qemu_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def qemu_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def qemu_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def spike_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def spike_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def spike_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def rocketchip_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def rocketchip_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def rocketchip_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def openocd_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def openocd_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def openocd_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def fesvr_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def fesvr_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def fesvr_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def linux_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def linux_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def linux_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def toolchain_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def toolchain_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def toolchain_deploy() {
+    sh("""
+echo 'Deploy' && 
 /bin/true
 """)
 }

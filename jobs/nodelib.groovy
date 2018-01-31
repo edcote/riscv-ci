2bd4ed41b9099700258035c3f4852ddda3ee580e
Edmond Cote <edmond.cote@gmail.com>
2018-01-31 16:02:52 +0000

    Initial version

---
+++ jobs/nodelib.groovy
@@ -0,0 +1,137 @@
+// DO NOT EDIT, MANAGED FILE
+def toolchain_build() {
+    sh("""
+/bin/true
+""")
+}
+def toolchain_test() {
+    sh("""
+/bin/true
+""")
+}
+def toolchain_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def spike_build() {
+    sh("""
+/bin/true
+""")
+}
+def spike_test() {
+    sh("""
+/bin/true
+""")
+}
+def spike_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def linux_build() {
+    sh("""
+/bin/true
+""")
+}
+def linux_test() {
+    sh("""
+/bin/true
+""")
+}
+def linux_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def openocd_build() {
+    sh("""
+/bin/true
+""")
+}
+def openocd_test() {
+    sh("""
+/bin/true
+""")
+}
+def openocd_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def tests_build() {
+    sh("""
+/bin/true
+""")
+}
+def tests_test() {
+    sh("""
+/bin/true
+""")
+}
+def tests_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def fesvr_build() {
+    sh("""
+/bin/true
+""")
+}
+def fesvr_test() {
+    sh("""
+/bin/true
+""")
+}
+def fesvr_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def rocketchip_build() {
+    sh("""
+/bin/true
+""")
+}
+def rocketchip_test() {
+    sh("""
+/bin/true
+""")
+}
+def rocketchip_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def qemu_build() {
+    sh("""
+/bin/true
+""")
+}
+def qemu_test() {
+    sh("""
+/bin/true
+""")
+}
+def qemu_deploy() {
+    sh("""
+/bin/true
+""")
+}
+def pk_build() {
+    sh("""
+/bin/true
+""")
+}
+def pk_test() {
+    sh("""
+/bin/true
+""")
+}
+def pk_deploy() {
+    sh("""
+/bin/true
+""")
+}
+return this;

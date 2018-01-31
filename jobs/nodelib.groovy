efdc6605c65b4e66c969e9c8b5f113e58cdf3286
Edmond Cote <edmond.cote@gmail.com>
2018-01-31 22:23:08 +0000

    add toolchain_linux

--- jobs/nodelib.groovy
+++ jobs/nodelib.groovy
@@ -17,26 +17,6 @@ echo 'Deploy' &&
 /bin/true
 """)
 }
-def pk_build() {
-    sh("""
-echo 'Build' && 
-rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
-../configure --prefix=$RISCV && 
-make
-""")
-}
-def pk_test() {
-    sh("""
-echo 'Test' && 
-/bin/true
-""")
-}
-def pk_deploy() {
-    sh("""
-echo 'Deploy' && 
-cd $WORKSPACE/build && make install
-""")
-}
 def qemu_build() {
     sh("""
 echo 'Build' && 
@@ -50,42 +30,6 @@ echo 'Test' &&
 """)
 }
 def qemu_deploy() {
-    sh("""
-echo 'Deploy' && 
-/bin/true
-""")
-}
-def tests_build() {
-    sh("""
-echo 'Build' && 
-/bin/true
-""")
-}
-def tests_test() {
-    sh("""
-echo 'Test' && 
-/bin/true
-""")
-}
-def tests_deploy() {
-    sh("""
-echo 'Deploy' && 
-/bin/true
-""")
-}
-def rocketchip_build() {
-    sh("""
-echo 'Build' && 
-/bin/true
-""")
-}
-def rocketchip_test() {
-    sh("""
-echo 'Test' && 
-/bin/true
-""")
-}
-def rocketchip_deploy() {
     sh("""
 echo 'Deploy' && 
 /bin/true
@@ -109,27 +53,7 @@ echo 'Deploy' &&
 /bin/true
 """)
 }
-def toolchain_build() {
-    sh("""
-echo 'Build' && 
-rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
-../configure --prefix=$RISCV && 
-make linux
-""")
-}
-def toolchain_test() {
-    sh("""
-echo 'Test' && 
-/bin/true
-""")
-}
-def toolchain_deploy() {
-    sh("""
-echo 'Deploy' && 
-cd $WORKSPACE/build && make install
-""")
-}
-def fesvr_build() {
+def pk_build() {
     sh("""
 echo 'Build' && 
 rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
@@ -137,13 +61,13 @@ rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build &&
 make
 """)
 }
-def fesvr_test() {
+def pk_test() {
     sh("""
 echo 'Test' && 
 /bin/true
 """)
 }
-def fesvr_deploy() {
+def pk_deploy() {
     sh("""
 echo 'Deploy' && 
 cd $WORKSPACE/build && make install
@@ -164,6 +88,102 @@ echo 'Test' &&
 """)
 }
 def spike_deploy() {
+    sh("""
+echo 'Deploy' && 
+cd $WORKSPACE/build && make install
+""")
+}
+def tests_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def tests_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def tests_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def toolchain_linux_build() {
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+../configure --prefix=$RISCV --enable-multilib && 
+make linux
+""")
+}
+def toolchain_linux_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def toolchain_linux_deploy() {
+    sh("""
+echo 'Deploy' && 
+cd $WORKSPACE/build && make install
+""")
+}
+def toolchain_newlib_build() {
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+../configure --prefix=$RISCV && 
+make
+""")
+}
+def toolchain_newlib_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def toolchain_newlib_deploy() {
+    sh("""
+echo 'Deploy' && 
+cd $WORKSPACE/build && make install
+""")
+}
+def rocketchip_build() {
+    sh("""
+echo 'Build' && 
+/bin/true
+""")
+}
+def rocketchip_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def rocketchip_deploy() {
+    sh("""
+echo 'Deploy' && 
+/bin/true
+""")
+}
+def fesvr_build() {
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+../configure --prefix=$RISCV && 
+make
+""")
+}
+def fesvr_test() {
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+}
+def fesvr_deploy() {
     sh("""
 echo 'Deploy' && 
 cd $WORKSPACE/build && make install

5fd58d0d83168496257e3d1411d474a65aa41792
Edmond Cote <edmond.cote@gmail.com>
2018-02-01 01:19:38 +0000

    Initial version

---
+++ pipelines/toolchain_linux_pipeline.groovy
@@ -0,0 +1,31 @@
+node {
+def nodelib = load("${env.RISCV_CI}/jobs/nodelib.groovy")
+
+stage('Clone') {
+    checkout([ $class: 'GitSCM',
+                    branches: [[name: '*/master']],
+                    userRemoteConfigs: [[url: 'https://github.com/riscv/riscv-gnu-toolchain']],
+                    extensions: [ [$class: 'SubmoduleOption', recursiveSubmodules: true, disableSubmodules: false, timeout: 120] ]
+             ])
+    sh('sleep 2s')
+}
+
+stage('Build') {
+    sh('printenv')
+    nodelib.toolchain_linux_build()
+    sh('sleep 2s')
+}
+
+stage('Test') {
+    sh('printenv')
+    nodelib.toolchain_linux_test()
+    sh('sleep 2s')
+}
+
+stage('Deploy') {
+    sh('printenv')
+    nodelib.toolchain_linux_deploy()
+    sh('sleep 2s')
+}
+}
+    

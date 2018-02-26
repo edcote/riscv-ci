0eeeb09b96b23ed0dddd1239b42c7f1cf7833517
Edmond Cote <edmond.cote@gmail.com>
2018-02-26 19:03:33 +0000

    update generated files

--- jobs/nodelib.groovy
+++ jobs/nodelib.groovy
@@ -1,222 +1,219 @@
 // DO NOT EDIT, MANAGED FILE
-def rocketchip_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-/bin/true
-""")
-}
-def rocketchip_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def rocketchip_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-/bin/true
-""")
-}
-def qemu_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-/bin/true
-""")
-}
-def qemu_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def qemu_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-/bin/true
-""")
-}
-def fesvr_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
-../configure --prefix=$RISCV && 
-make
-""")
-}
-def fesvr_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def fesvr_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-cd $WORKSPACE/build && make install
-""")
-}
-def toolchain_newlib_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
-../configure --prefix=$RISCV && 
-make
-""")
-}
-def toolchain_newlib_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def toolchain_newlib_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-cd $WORKSPACE/build && make install
-""")
-}
-def tests_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-/bin/true
-""")
-}
-def tests_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def tests_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-/bin/true
-""")
-}
-def linux_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-/bin/true
-""")
-}
-def linux_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def linux_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-/bin/true
-""")
-}
-def spike_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
-../configure --prefix=$RISCV --with-fesvr=$RISCV && 
-make
-""")
-}
-def spike_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def spike_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-cd $WORKSPACE/build && make install
-""")
-}
-def toolchain_linux_build() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Build' && 
-mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
-../configure --prefix=$RISCV && 
-make linux
-""")
-}
-def toolchain_linux_test() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Test' && 
-/bin/true
-""")
-}
-def toolchain_linux_deploy() {
-    sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-cd $WORKSPACE/build && make install
-""")
-}
 def pk_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
     sh("""
-export MAKEFLAGS=-j8
 echo 'Build' && 
-mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
-../configure --prefix=$RISCV && 
-make
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH ../configure --prefix=$WORKSPACE/riscv-root --host=riscv64-unknown-elf && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH make -j4 && 
+make install
 """)
+    }
 }
+
+
 def pk_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
     sh("""
-export MAKEFLAGS=-j8
 echo 'Test' && 
 /bin/true
 """)
+    }
 }
-def pk_deploy() {
+
+
+def toolchain_linux_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
     sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
-cd $WORKSPACE/build && make install
-""")
-}
-def openocd_build() {
-    sh("""
-export MAKEFLAGS=-j8
 echo 'Build' && 
-/bin/true
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH ../configure --prefix=$WORKSPACE/riscv-root && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH make -j4 linux
 """)
+    }
 }
-def openocd_test() {
+
+
+def toolchain_linux_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
     sh("""
-export MAKEFLAGS=-j8
 echo 'Test' && 
 /bin/true
 """)
+    }
 }
-def openocd_deploy() {
+
+
+def qemu_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
     sh("""
-export MAKEFLAGS=-j8
-echo 'Deploy' && 
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build
+""")
+    }
+}
+
+
+def qemu_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
 /bin/true
 """)
+    }
 }
+
+
+def fesvr_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH ../configure --prefix=$WORKSPACE/riscv-root && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH make -j4 && 
+make install
+""")
+    }
+}
+
+
+def fesvr_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+    }
+}
+
+
+def tests_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH ../configure --prefix=$WORKSPACE/riscv-root && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH make -j4 && 
+make install
+""")
+    }
+}
+
+
+def tests_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+    }
+}
+
+
+def linux_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build
+""")
+    }
+}
+
+
+def linux_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+    }
+}
+
+
+def rocketchip_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build
+""")
+    }
+}
+
+
+def rocketchip_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+    }
+}
+
+
+def spike_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH ../configure --prefix=$WORKSPACE/riscv-root --with-fesvr=$WORKSPACE/riscv-root && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH make -j4 && 
+make install
+""")
+    }
+}
+
+
+def spike_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+    }
+}
+
+
+def toolchain_newlib_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/build && cd $WORKSPACE/build && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH ../configure --prefix=$WORKSPACE/riscv-root && 
+PATH=$WORKSPACE/riscv-root/bin:$PATH make -j4 && 
+make install
+""")
+    }
+}
+
+
+def toolchain_newlib_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+    }
+}
+
+
+def openocd_build() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Build' && 
+rm -rf $WORKSPACE/build && mkdir -p $WORKSPACE/riscv-root && cd $WORKSPACE/build
+""")
+    }
+}
+
+
+def openocd_test() {
+    withEnv(["PATH=${env.WORKSPACE}/riscv-root/bin:${env.PATH}"]) { 
+    sh("""
+echo 'Test' && 
+/bin/true
+""")
+    }
+}
+
+
 return this;

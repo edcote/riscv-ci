f193dc9d382b69d049dc6c7b7f084ba5497d342a
Edmond Cote <edmond.cote@gmail.com>
2018-02-26 19:03:04 +0000

    copy artifacts from previous build, set environment before running build/test

--- jobs/pipegen.py
+++ jobs/pipegen.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
 import json
+import sys
 
 jsonfile = "pipespec.json"
 
 pipespec = json.load(open(jsonfile))
 
+def copyA(x):
+    return "    copyArtifacts filter: '**/*', fingerprintArtifacts: true, projectName: '{}', selector: lastSuccessful()".format(x)
+
 # pipeline pipelines
 for pipe in pipespec:
     f = open("../pipelines/{}_pipeline.groovy".format(pipe), 'w')
+
+    copyArtifacts = "\n".join(list(map(copyA, pipespec[pipe]['dependencies'])))
+
     f.write("""\
 properties([
 buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')),
@@ -19,31 +26,36 @@ node {{
 def nodelib = load("${{env.RISCV_CI}}/jobs/nodelib.groovy")
 
 stage('Clone') {{
-    sh('printenv')
     checkout([ $class: 'GitSCM',
                     branches: [[name: '*/master']],
                     userRemoteConfigs: [[url: 'https://github.com/{github}']],
                     extensions: [ [$class: 'SubmoduleOption', recursiveSubmodules: true, disableSubmodules: false, timeout: 120] ]
              ])
-    sh('sleep 2s')
+    sh('sleep 0.1s')
+}}
+
+stage('Dependencies') {{
+    {copyArtifacts}
+    sh('sleep 0.1s')
 }}
 
 stage('Build') {{
+    sh('printenv')
     nodelib.{pipe}_build()
-    sh('sleep 2s')
+    sh('sleep 0.1s')
 }}
 
 stage('Test') {{
     nodelib.{pipe}_test()
-    sh('sleep 2s')
+    sh('sleep 0.1s')
 }}
 
 stage('Archive') {{
-    nodelib.{pipe}_archive()
-    sh('sleep 2s')
+    archiveArtifacts artifacts: 'riscv-root/**/*', excludes: ''
+    sh('sleep 0.1s')
 }}
 }}
-    """.format(**{'github': pipespec[pipe]['github'], 'pipe': pipe}))
+    """.format(**{'github': pipespec[pipe]['github'], 'pipe': pipe, 'copyArtifacts': copyArtifacts}))
     f.close
 
 # dummy pipelines
@@ -54,17 +66,13 @@ for pipe in pipespec:
     for stage in ["build", "test"]:
         f.write("""\
 def {}_{}() {{
+    withEnv(["PATH=${{env.WORKSPACE}}/riscv-root/bin:${{env.PATH}}"]) {{ 
     sh(\"\"\"
 {}
 \"\"\")
+    }}
 }}
-""".format(pipe, stage, " && \n".join(pipespec[pipe][stage])))
-    # archiveArtifacts
-    for stage in ["archive"]:
-        f.write("""\
-def {}_{}() {{
-    archiveArtifacts artifacts: 'build/riscv/**/*', excludes: ''
-}}
+
 
 """.format(pipe, stage, " && \n".join(pipespec[pipe][stage])))
 
